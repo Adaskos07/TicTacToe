91f6158dcd8ba6e2957f163979d6d68d2aa4c955
Adaskos07 <a.sulak@protonmail.com>
2019-05-15 21:25:44 +0200

    new files

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <bool.h>
+
+int main()
+{
+	bool isAgainstAI = false; //flag that will be used to choose correct game mode
+	char userChoice;
+	int gameBoad[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	while (1) {
+		//user input
+		//data processing
+		//terminal output
+		break;
+	}
+
+	return 0;
+}
