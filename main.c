ca18527c6a879a87e33483c3f8c4ac5ac093326e
Adaskos07 <a.sulak@protonmail.com>
2019-05-16 00:45:13 +0200

    new code in main.c

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
-#include <bool.h>
+#include <stdbool.h>
 
 int main()
 {
 	bool isAgainstAI = false; //flag that will be used to choose correct game mode
 	char userChoice;
-	int gameBoad[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+	int gameBoad[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	printf("Welcome to the Tic Tac Toe game!!!\n\
+			\rWould you like to play with a computer or another player?\n\
+			\rTo play with the computer type \"C\"\n\
+			\rTo play with a partner type \"H\"\n"
+		  );
+
+	scanf("%c", &userChoice);
+
+	if (userChoice == 'c' || userChoice == 'C')
+		isAgainstAI == true;
+	else
+		isAgainstAI == false;
+	
+	printf("\n%c", userChoice);
 
 	while (1) {
 		//user input
