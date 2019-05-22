0d499adbb49ff12fdf5f9c5f373740c01fae8d09
Adaskos07 <a.sulak@protonmail.com>
2019-05-22 13:34:53 +0200

    some cleaning

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CIRCLE 0
+#define CROSS 1
 
 
 void printPromptAI() {
 	printf("Welcome to the Tic Tac Toe game!!!\n\
 			\rWould you like to play with a computer or another player?\n"
-		  ); // add large sign with AI or another player, choice use with a number subtracted by 1
+		  ); // add large sign with AI or another player, 
+	         // choice use with a number subtracted by 1
 }
 
 
-bool isAIplaying(int userChoice) {
+bool isAIplaying() {
+	int userChoice;
+	scanf("%d", &userChoice);
 	--userChoice;
 	if (!userChoice) // choice has to be deacreased by one 
 		return true; // first option is AI !(1 - 1 = 0) = true
@@ -24,77 +32,90 @@ void printPromtOX() {
 }
 
 
-void getUserSymbol(char player, char other) {
-	int choice
-	scanf("%c", &choice);
+void getUserSymbol(int *player, int  *other) {
+	int choice;
+	scanf("%d", &choice);
 	--choice;
 	//check choice
-	//mechanism to check if input is correct if letter is small use toUpper
-	if (choice) 
-		player = "X";
-		other == "O";
-	else
-		player = "O";
-		other == "X";
+	if (choice) {
+		*player = CIRCLE;
+		*other = CROSS;
+	}
+	else {
+		*player = CROSS;
+		*other = CIRCLE;
+	}
 }
 
 
-void updateGameBoard(int move, int[] board, int bsize, int userNumber);
+void updateGameBoard(int move, int board[], int bsize) {
+}
 
 
-bool isMoveAllowed(int move, int[] board, int bsize);
+bool isMoveAllowed(int move, int board[], int bsize) {
+}
 
 
-void printBoard(int[] board, int bsize);
+void printBoard(int board[], int bsize);
 
 
-bool makeAImove(int[] board, int bsize);
+void makeAImove(int board[], int bsize) {
+	int move = rand() % 9;
+
+	while (!isMoveAllowed(move, board, bsize))
+		move = rand() % 9;
+
+	updateGameBoard(move, board, bsize);
+}
 
 
-bool isGameWon(int[] board) {
+bool isGameWon(int board[]) {
 	// 012 345 678 horizontal lines
 	// 036 147 258 vertical lines 
 	// 048 246     diagonals
 	// ignore zeroes! emptyy element
+	return false;
 }
 
-
-
+/*
+X    X         OO
+ X  X         O  O
+  XX         O    O
+ X  X         O  O
+X    X         OO
+*/
 
 int main()
 {
+	srand(time(NULL));
 	bool isAgainstAI; //flag that will be used to choose correct game mode
-	char player1; player2;
+	int player1, player2;
+
+	int gameBoad[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	bool counterEven;
 
 	printPromptAI();
-	isAgainstAI = isAIplaying(userChoice);
+	isAgainstAI = isAIplaying();
 
 	printPromtOX();
-	getUserSymbol(player1, player2);
+	getUserSymbol(&player1, &player2);
 
 	//save O or X
     //promt lets begin game
 	//clear the screen
 
-	printf("\n%c", userChoice);
-	int counter = 0;
 	
-	int gameBoad[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-	bool counterEven, 
-
-
-
-
-		// PRINT THE BOARD
-
+	int counter = 0;
 	while (1) {
 
 		counterEven = counter % 2 ? true : false;
 
-		if (isGameWon()) {
+		if (isGameWon(gameBoad)) {
 			// congrats and result prompt
 			break; //quit
 		}
+		break;
+
 			 
 
 		//   check is the game is won,remember the counter
