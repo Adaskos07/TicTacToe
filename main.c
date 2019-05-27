a582e2075021361ccda30c4ca7fdc28c33b1998f
Adaskos07 <a.sulak@protonmail.com>
2019-05-27 18:35:18 +0200

    update to AI functions

--- main.c
+++ main.c
@@ -7,6 +7,26 @@
 #define CROSS 1
 
 
+void printWelcome() {
+	char letter;
+	FILE *fptr;
+	fptr = fopen("welcome.txt", "r");
+
+	if (fptr == NULL) {
+		printf("Error!");
+		fclose(fptr);
+		return;
+	}
+
+	while (fscanf(fptr, "%c", &letter) != EOF) {
+		printf("%c", letter);
+	}
+
+	fclose(fptr);
+	return;
+}
+
+
 void printPromptAI() {
 	printf("Welcome to the Tic Tac Toe game!!!\n\
 			\rWould you like to play with a computer or another player?\n"
@@ -20,7 +40,6 @@ bool isAIplaying() {
 	scanf("%d", &userChoice);
 	--userChoice;
 
-
 	if (!userChoice) // choice has to be deacreased by one 
 		return true; // first option is AI !(1 - 1 = 0) = true
 	else
@@ -29,8 +48,14 @@ bool isAIplaying() {
 
 
 void printPromtOX() {
-	printf("Player ONE!!! Chose symbol! X or O?\n");
-	//print large X and large O with numbers 1 and 2 unerneath
+	printf("Player ONE!!! Chose symbol!\n");
+	printf("X    X         OOOO\n\
+	      \r X  X         O    O\n\
+		  \r  XX          O    O\n\
+		  \r X  X         O    O\n\
+		  \rX    X         OOOO\n\n\
+		  \r (1)     or   (2)\n"
+			 );
 }
 
 
@@ -50,10 +75,9 @@ void getUserSymbol(int *player, int  *other) {
 }
 
 
-void printGameBagin() {
+void printGameBegin() {
 	//lets begin game message
 	// clear the screen
-
 
 }
 
@@ -77,16 +101,16 @@ bool isMoveAllowed(int move, int board[]) {
 }
 
 
-void printBoard(int board[], int bsize);
+void printBoard(int board[], int bsize) {
+}
 
 
-void makeAImove(int board[], int AIsymbol) {
+int makeAImove() {
 	int move = rand() % 9;
-
 	while (!isMoveAllowed(move, board))
 		move = rand() % 9;
 
-	updateGameBoard(AIsymbol, move, board);
+	return move;
 }
 
 
@@ -102,18 +126,20 @@ bool isGameWon(int board[]) {
 int main()
 {
 	srand(time(NULL));
-	
-	//flag that will be used to choose correct game mode, true - player 1, false -player 2
-	bool isAgainstAI, playerTurn;
-
+	//flag that will be used to choose correct game mode,
+	bool isAgainstAI;
+	//true - player 1, false -player 2
+	bool playerTurn;
 	//saves the chosen symbol, 0 circle, 1 cross
 	int player1, player2; 
 	//-1 empty, 0 circle, 1 cross
 	int gameBoard[9] = { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
-	int playerChoice;
+	// saves moves between 1 and 9
+	int nextMove; 
 
-	//printPromptAI();
-	//isAgainstAI = isAIplaying();
+	printWelcome();
+	printPromptAI();
+	isAgainstAI = isAIplaying();
 
 	printPromtOX();
 	getUserSymbol(&player1, &player2);   //save O or X
@@ -121,12 +147,13 @@ int main()
 
     //promt lets begin game
 	//clear the screen
-
 	
 	int counter = 0;
-	while (1) {
+	bool currentPlayer;
+	while (counter < 10) {
 
 		playerTurn = counter % 2 ? false : true;
+		currentPlayer = playerTurn ? player1 : player2;
 
 		//if (isGameWon(gameBoard)) {
 			// congrats and result prompt
@@ -134,32 +161,30 @@ int main()
 		//}
 
 		if (playerTurn)
-			printf("player 1 move!");
+			printf("Player 1! Make a move!\n");
+			
+		else if (isAgainstAI) {
+			printf("AI makes a move...");
+			nextMove = makeAImove();
+		}
 		else
-			printf("player 2 move!");
+			printf("Player 2! Make a move!\n");
+			
 
 		
-		scanf("%d", &playerChoice);
+		scanf("%d", &nextMove);
 
-		while (!isMoveAllowed(playerChoice, gameBoard)) {
-			printf("WRONG INPUT, try again!");
-			scanf("%d", &playerChoice);
+		while (!isMoveAllowed(nextMove, gameBoard)) {
+			printf("WRONG INPUT, try again!\n");
+			scanf("%d", &nextMove);
 		}
 
-		if (playerTurn)
-			updateGameBoard(playerChoice, gameBoard, player1);
-		else
-			updateGameBoard(playerChoice, gameBoard, player2);
+		updateGameBoard(nextMove, gameBoard, currentPlayer);
 			
-
 		for (int i = 0; i < 9; ++i)
 			printf("%d  ", gameBoard[i]);
+		printf("\n");
 			
-
-
-
-			 
-
 		//   check is the game is won,remember the counter
 		//   promt user 1 or 2 depending in the counter
 		//   take  input
